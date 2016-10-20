59732ed5a50c6aaba322b98bc09e77bb514fac36
Jyoti Prakash <j.prakash2198@gmail.com>
2016-10-20 22:17:19 +0530

    Update AsyncSyntax.java

--- AsyncSyntax.java
+++ AsyncSyntax.java
@@ -1,41 +1,28 @@
-    class MySumTask extends AsyncTask<Integer,Integer,Integer>
+    class MySumTask extends AsyncTask<Void,Void,Void>
     {
         @Override
         protected void onPreExecute() {
-            Toast.makeText(MainActivity.this, "onPreExucute", Toast.LENGTH_SHORT).show();
+           // code for initializing variables before execution
            // super.onPreExecute();
         }
 
         @Override
-        protected Integer doInBackground(Integer... integers) {
+        protected Integer doInBackground(Void... integers) {
             //All the BackGround work goes here
-            int n = integers[0];
-            int sum = 0;
-            for (int i = 1;i <= n;i++){
-                sum +=i;
-
-                publishProgress(i,n);
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            return sum;
+          
+               
+             return null;
         }
 
         @Override
-        protected void onProgressUpdate(Integer... values) {
+        protected void onProgressUpdate(Void... values) {
             super.onProgressUpdate(values);
-            int i = values[0];
-            int n = values[1];
-            double percentage = ((double)i/(double)n)*100;
-            Toast.makeText(MainActivity.this, ""+percentage, Toast.LENGTH_SHORT).show();
+            // pass data here for progress values
         }
 
         @Override
-        protected void onPostExecute(Integer integer) {
+        protected void onPostExecute(Void integer) {
             super.onPostExecute(integer);
-            textView.setText("S(n) ="+integer);
+          // return the result
         }
     }
